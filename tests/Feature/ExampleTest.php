5da1b395723135233abbfbbf051fd3d53273dd2f
Carlos Ferreira <carlos@especializati.com.br>
2023-12-27 10:04:26 -0300

    chore: install and config pest php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,5 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+it('should return true', function () {
+    expect(true)->toBe(true);
+});
