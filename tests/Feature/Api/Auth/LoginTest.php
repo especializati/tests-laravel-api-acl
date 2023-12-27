dc193ebec186c8dfd8742596d0448b19040dae54
Carlos Ferreira <carlos@especializati.com.br>
2023-12-27 10:40:15 -0300

    test: should fail authentication

--- tests/Feature/Api/Auth/LoginTest.php
+++ tests/Feature/Api/Auth/LoginTest.php
@@ -15,3 +15,23 @@ test('should auth user', function () {
         ->assertOk()
         ->assertJsonStructure(['token']);
 });
+
+test('should fail auth - with wrong password', function () {
+    $user = User::factory()->create();
+    $data = [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+        'device_name' => 'e2e_test',
+    ];
+    postJson(route('auth.login'), $data)->assertStatus(422);
+});
+
+test('should fail auth - with wrong email', function () {
+    $user = User::factory()->create();
+    $data = [
+        'email' => 'fake@email.com',
+        'password' => 'password',
+        'device_name' => 'e2e_test',
+    ];
+    postJson(route('auth.login'), $data)->assertStatus(422);
+});
