a10a27ea2f81cb816b9eda82316a7393886f997c
Carlos Ferreira <carlos@especializati.com.br>
2023-12-28 09:27:51 -0300

    test: validations login

--- tests/Feature/Api/Auth/LoginTest.php
+++ tests/Feature/Api/Auth/LoginTest.php
@@ -35,3 +35,38 @@ test('should fail auth - with wrong email', function () {
     ];
     postJson(route('auth.login'), $data)->assertStatus(422);
 });
+
+describe('validations', function () {
+    it('should require email', function () {
+        postJson(route('auth.login'), [
+            'password' => 'password',
+            'device_name' => 'e2e_test',
+        ])
+        ->assertJsonValidationErrors([
+            'email' => trans('validation.required', ['attribute' => 'email'])
+        ])
+        ->assertStatus(422);
+    });
+    it('should require password', function () {
+        $user = User::factory()->create();
+        postJson(route('auth.login'), [
+            'email' => $user->email,
+            'device_name' => 'e2e_test',
+        ])
+        ->assertJsonValidationErrors([
+            'password' => trans('validation.required', ['attribute' => 'password'])
+        ])
+        ->assertStatus(422);
+    });
+    it('should require device name', function () {
+        $user = User::factory()->create();
+        postJson(route('auth.login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ])
+        ->assertJsonValidationErrors([
+            'device_name' => trans('validation.required', ['attribute' => 'device name'])
+        ])
+        ->assertStatus(422);
+    });
+});
